8379296119091ffb5476799e3aa18e50710b6e80
Jisoo77 <sorento12547@gmail.com>
2024-11-07 16:58:13 +0900

    a241107

--- 1106/cgv/js/script.js
+++ 1106/cgv/js/script.js
@@ -4,7 +4,20 @@ $(function(){
     var list_w = ($('.slideBox').width()+32.5)*5;
     // alert(list_w);
     $('.next').click(function(){
+        if(num==3){
+            num=0;
+            $('.listWrap').css('margin-left','0');
+        }
         num++;
+        $('.listWrap').stop().animate({marginLeft:-list_w*num},500);
+        return false;
+    });
+    $('.prev').click(function(){
+        if(num==0){
+            num=3;
+            $('.listWrap').css('margin-left',-list_w*num);
+        }
+        num--;
         $('.listWrap').stop().animate({marginLeft:-list_w*num},500);
         return false;
     });
