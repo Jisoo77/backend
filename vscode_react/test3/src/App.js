a7ddcb146712e8bda41b0401804317739109aa1b
Jisoo77 <sorento12547@gmail.com>
2024-11-26 17:30:24 +0900

    a241126

--- vscode_react/test3/src/App.js
+++ vscode_react/test3/src/App.js
@@ -1,12 +1,14 @@
 import InputSample from "./InputSample";
 import React, { useState } from 'react';
+import MultiInputForm from "./MultiInputForm";
 function App(){
   const [likeCount, setLikeCount] = useState(0); // React 상태 관리
   return(
     <div>
       {/* <InputSample/> */}
-      <h1>좋아요 수: {likeCount}</h1>
-      <button onClick={() => setLikeCount(likeCount + 1)}>좋아요</button>
+      {/* <h1>좋아요 수: {likeCount}</h1>
+      <button onClick={() => setLikeCount(likeCount + 1)}>좋아요</button> */}
+      <MultiInputForm/>
     </div>
   );
 }
---
+++ vsjava/java/src/a1126/sec03/SmartPhone.java
@@ -0,0 +1,22 @@
+package a1126.sec03;
+
+public class SmartPhone extends Phone {
+    // 부모인 Phone으로부터 SmartPhone이 필드와 메소드를 상속받는다.
+    // 필드 선언
+    public boolean wifi;
+    
+    public SmartPhone(String model, String color){
+        super(model, color);
+        // 부모클래스의 생성자 호출
+    }
+
+    // setter 메소드
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+        System.out.println("와이파이 상태를 변경했습니다.");
+    }
+    public void internet(){
+        System.out.println("인터넷을 연결합니다.");
+    }
+
+}
